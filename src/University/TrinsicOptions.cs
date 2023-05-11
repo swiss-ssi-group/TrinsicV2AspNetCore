2c7778578edab8cbdf4050b6e44cb800201302ea
Matteo <matteo@farmerconnect.com>
2023-05-11 11:30:10 +0200

    Add solution and first project

---
+++ src/University/TrinsicOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace University;
+
+public class TrinsicOptions
+{
+  public const string Trinsic = "Trinsic";
+
+  public string Ecosystem { get; set; } = default!;
+  public string ApiKey { get; set; } = default!;
+}
+
